a92ef7af3af29559894ded688cc643d4cd72b16f
Alejandro <acuellarh@gmail.com>
2022-04-28 19:39:29 -0500

    Add Authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+
+const CLIENT_ID = "da41273bb2444d84a74e22c37b8e4554"
+const REDIRECT_URI = "http://localhost:3000"
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+const RESPONSE_TYPE = "token"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+      <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
       </header>
     </div>
   );
